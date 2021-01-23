fb59b70dbd6ddf428136e2341d7cb013beb1878c
Muaz Akhtar <70416243+muaz208@users.noreply.github.com>
2021-01-23 00:44:27 -0500

    Add files via upload

---
+++ QuadraticEquation.py
@@ -0,0 +1,31 @@
+import math
+def quadratic():
+    
+    try:
+        
+        a=int(input("Please enter the a value: "))
+        b=int(input("Please enter the a value: "))
+        c=int(input("Please enter the a value: "))
+    except:
+        print("Not a valid entry, please try again")
+        quadratic()
+        return None
+    
+    discrim=(b*b)-(4*a*c)
+    
+    if(discrim>0):
+        x1=((-1*b)+math.sqrt(discrim))//(2*a)
+        x2=((-1*b)-math.sqrt(discrim))//(2*a)
+        print("\nThe two roots are x1=", x1, "and x2=",x2)
+        return x1,x2
+    
+    elif(discrim==0):
+        x=(-1*b)//(2*a);
+        print("The only root is x=",x)
+        return x
+    
+    elif(discrim<0):
+        print("There are no real roots!")
+        return None
+        
+quadratic()
